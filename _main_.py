52f4ae898d1f1eb0188a1e878497c86285268567
luk0y <45989946+luk0y@users.noreply.github.com>
2019-11-18 12:28:19 +0530

    Add files via upload

---
+++ _main_.py
@@ -0,0 +1,44 @@
+import speech_recognition as sr
+from queries import *
+from subprocess import call
+import pyttsx3
+import time 
+
+engine = pyttsx3.init()
+r = sr.Recognizer()
+engine.say("Hello boss, Welcome to my project")
+engine.runAndWait()
+time.sleep(1)
+engine.say("Tell me what I have to do boss")
+engine.runAndWait()
+while True:
+	with sr.Microphone() as source:
+		r.adjust_for_ambient_noise(source)
+		print("Say Lock/Unlock the door\n")
+		audio=r.listen(source)
+		print("Recorded\n")
+	try:
+		userdata=r.recognize_google(audio).lower()
+		print("You said:- " + r.recognize_google(audio))
+		if(userdata in unlock):
+			call(["touch", "success"])
+			call(["scp","success","pi@192.168.4.1:/home/pi/Desktop/project/output"])
+			call(["rm", "success"])
+			engine.say('Door Unlocked')
+			engine.runAndWait()
+		elif(userdata in lock):
+			call(["touch", "fail"])
+			call(["scp","fail","pi@192.168.4.1:/home/pi/Desktop/project/output"])
+			call(["rm", "fail"])
+			engine.say('Door locked')
+			engine.runAndWait()
+		else:
+			engine.say('I am not human to understand whatever you say')
+			engine.runAndWait()
+	except sr.UnknownValueError:
+         	 print("Could not understand audio")
+	
+
+
+
+
---
+++ queries.py
@@ -0,0 +1,4 @@
+lock=['lock the door','lock it']
+unlock = ['unlock the door','unlock','unlock unlock unlock','remove lock']
+
+
