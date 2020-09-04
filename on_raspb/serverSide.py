53a257e91ca77a20e65968621ccc8908f512a4cb
luk0y <45989946+luk0y@users.noreply.github.com>
2020-09-04 09:51:17 +0530

    Update serverSide.py

--- on_raspb/serverSide.py
+++ on_raspb/serverSide.py
@@ -1,14 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 import RPi.GPIO as GPIO
 import time
 from subprocess import call,check_output
+
+
+
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setwarnings(False)
+
 GPIO.setup(12,GPIO.OUT)
 
 while(True):
-	mad = check_output(["ls","test"])
-	string = mad.decode("utf-8")
+	bool_test = check_output(["ls","test"])
+	string = bool_test.decode("utf-8")
 	string  = string[:-1]
 	if(string=="success"):
         	GPIO.output(12,GPIO.LOW)
